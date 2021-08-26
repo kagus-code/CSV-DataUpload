0fcf827688b7463011ca5ff7cccad5cf50f16378
Eston Kagwima <ekagwima745@gmail.com>
2021-08-26 14:19:46 +0300

    configure media uploads

--- datafull/urls.py
+++ datafull/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -13,3 +15,6 @@ urlpatterns = [
 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
