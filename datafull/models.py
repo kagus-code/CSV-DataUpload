4fcfc3ddbf65524110318539f22e030a2cfa4d0b
Eston Kagwima <ekagwima745@gmail.com>
2021-08-26 18:14:58 +0300

    adding the sale model and migrating it

--- datafull/models.py
+++ datafull/models.py
@@ -8,3 +8,21 @@ class Csv(models.Model):
 
   def __str__(self):
     return f'File id {self.id}'
+
+
+class Sale(models.Model):
+  InvoiceNo = models.IntegerField(null=True , blank=True)
+  StockCode= models.CharField(max_length=200, null=True, blank=True,unique=True)
+  Description= models.CharField(max_length=255,null=True,blank=True)
+  Quantity = models.PositiveIntegerField(null=True , blank=True, default=0)
+  InvoiceDate= models.CharField(max_length=200, null=True, blank=True,)
+  UnitPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+  CustomerID=models.IntegerField(null=True , blank=True)
+  Country= models.CharField(max_length=255,null=True,blank=True)
+
+  def __str__(self):
+        return str(self.StockCode)
+
+
+  def save_sale(self):
+        self.save()
